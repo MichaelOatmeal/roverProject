929d3b4c61d742a96875af9a4054dde7d97a0148
Michael Oatmeal <78619127+MichaelOatmeal@users.noreply.github.com>
2026-01-28 08:47:58 +0000

    PlatformIO Integration
    Added Arduino test code [active-buzzer.cpp] and related libraries using VS Code's PlatformIO extension

---
+++ arduino/test/test_active-buzzer/active-buzzer.cpp
@@ -0,0 +1,35 @@
+#include "Arduino.h"
+#include "pitches.h"
+ 
+// notes in the melody:
+int melody[] = {
+  NOTE_C5, NOTE_C5, NOTE_C6, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, NOTE_C5, NOTE_DS5, NOTE_F5};
+
+int durations[] = {
+  250, 250, 500, 750, 500, 500, 500, 250, 250, 250
+};
+
+int melodyNoteOne[] = {
+  NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_GS4
+};
+
+int duration = 0;
+ 
+void setup() {
+};
+ 
+void loop() { 
+  for (int startNote = 0; startNote < 4; startNote++) {
+    melody[0] = melodyNoteOne[startNote];
+    melody[1] = melodyNoteOne[startNote];
+
+    for (int thisNote = 0; thisNote < 10; thisNote++) {
+      duration = durations[thisNote]*0.55;
+      // pin8 output the voice
+      tone(8, melody[thisNote], duration);
+      
+      // Output the voice after several minutes
+      delay(duration);
+    }
+  }
+}
