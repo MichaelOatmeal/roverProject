71600319e25ed1e746c54d4cebcf81cccb463bf6
Michael Oatmeal <78619127+MichaelOatmeal@users.noreply.github.com>
2026-02-16 16:15:12 +0000

    Create serial-control.cpp
    Create serial-control.cpp
    Update test.py with packet handling
    Amended .gitignore with C++ info

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,31 +1,14 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
 #include "Arduino.h"
+#include "serial-control.h"
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
-
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+  serialInit();
+  pinMode(LED_PIN, OUTPUT);  // onboard LED has no pwm
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+void loop() {
+  serialRead();
+  if(target_Id == 0x01) {
+    analogWrite(LED_PIN, target_Led_Pwm);
+  }
 }
--- ground-controller/test.py
+++ ground-controller/test.py
@@ -11,29 +11,30 @@ pygame.display.flip()  # update screen
 pygame.display.set_caption("Input Window")
 
 # serial setup
-ser = serial.Serial(port='COM3', baudrate=115200)  # open serial port @ 115200 baud
-print(ser.name)         # check which port was really used
+ser = serial.Serial(port='COM4', baudrate=115200)  # open serial port @ 115200 baud
+print("Serial port: " + ser.name + "\nBaud: " + str(ser.baudrate))  # check which port and baud was really used
+
 
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            ser.close()
+            ser.close()  # close connection before killing
             running = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        ser.write(b'w')
-        print("w")
+    if keys[pygame.K_w]:  # send packet over serial
+        packet = bytes([0xFF, 0x01, 0xFF])  # led on
+        ser.write(packet)
     if keys[pygame.K_s]:
-        ser.write(b's')
-        print("s")
-    if keys[pygame.K_a]:
-        ser.write(b'a')
-        print("a")
-    if keys[pygame.K_d]:
-        ser.write(b'd')
-        print("d")
+        packet = bytes([0xFF, 0x01, 0x00])  # led off
+        ser.write(packet)
+    if keys[pygame.K_z]:
+        packet = bytes([0x4A, 0x01, 0x00])  # bad header test
+        ser.write(packet)
+    if keys[pygame.K_x]:
+        packet = bytes([0xFF, 0xB3, 0xFF])  # unused id test
+        ser.write(packet)
 
 pygame.quit()
